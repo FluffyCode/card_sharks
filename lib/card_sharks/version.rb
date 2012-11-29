aa3d2bd5ea1c11a638234abeadf983eaa9523189
Anthony Severini <FluffyCode@gmail.com>
2012-11-29 10:21:28 -0800

    Update version file.

--- lib/card_sharks/version.rb
+++ lib/card_sharks/version.rb
@@ -1,6 +1,9 @@
 module CardSharks
-  VERSION = "2.0.0"
+  VERSION = "2.0.1"
 end
 
 # v1.0.0 = Blackjack game.
 # v2.0.0 = inclusion of Go Fish game.
+
+# Begin including add_a_card game.
+# Begin including "War" game (on hold).
