336182333c8b3586e65cdad01a22d7a6483006f3
Anthony Severini <FluffyCode@gmail.com>
2014-08-23 14:10:03 -0700

    Reindent

--- lib/master/deck.rb
+++ lib/master/deck.rb
@@ -1,54 +1,54 @@
 require_relative "card"
 
 class Deck
-	SIZE = 52
+  SIZE = 52
 
-	def initialize 
-		@deck = []
+  def initialize 
+    @deck = []
 
-		SIZE.times do |i|
-			rank = Card.ranks[i % 13]
-			suit = Card.suits[i % 4]
+    SIZE.times do |i|
+      rank = Card.ranks[i % 13]
+      suit = Card.suits[i % 4]
 
-			@deck << Card.new(suit, rank)
-		end
-	end
+      @deck << Card.new(suit, rank)
+    end
+  end
 
-	def self
-		@deck
-	end
+  def self
+    @deck
+  end
 
-	def delete(x)
-		@deck.delete(x)
-	end
+  def delete(x)
+    @deck.delete(x)
+  end
 
-	def deck(x)
-		@deck[x]
-	end
+  def deck(x)
+    @deck[x]
+  end
 
-	def tell_deck
-		@deck.join(", ")
-	end
+  def tell_deck
+    @deck.join(", ")
+  end
 
-	def sort!
-		@deck.sort_by do |card|
-			card.suit
-		end
-	end
+  def sort!
+    @deck.sort_by do |card|
+      card.suit
+    end
+  end
 
-	def shuffle!
-		@deck.shuffle!
-	end
+  def shuffle!
+    @deck.shuffle!
+  end
 
-	def remove_top_card
-		@deck.delete_at(0)
-	end
+  def remove_top_card
+    @deck.delete_at(0)
+  end
 
-	def add_card_to_deck(card)
-		@deck << card
-	end
+  def add_card_to_deck(card)
+    @deck << card
+  end
 
-	def length
-		@deck.length
-	end
+  def length
+    @deck.length
+  end
 end
